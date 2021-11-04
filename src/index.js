1eb48b905843fc0f11a042c0b440415b19217f1e
Sajan Gutta <gutta@usc.edu>
2021-11-03 20:11:37 -0700

    Adding hash router to handle routing on gh pages.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux"
+import { HashRouter } from 'react-router-dom'
 import store from "./redux/store"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
