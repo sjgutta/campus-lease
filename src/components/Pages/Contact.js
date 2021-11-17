e5c3de2dd626939fad42bc65472572a42aeae706
Max Friedman <max.friedman0@gmail.com>
2021-11-16 18:32:09 -0800

    better form

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Page from "../Page/Page"
 import ContactBanner from "../../images/contactbanner.png";
 import "./Contact.css"
+import InputHeader from "../Input/InputHeader"
+import Input from "../Input/Input"
 
 
 const Contact = () => {
@@ -12,26 +14,15 @@ const Contact = () => {
                 <img src={ContactBanner} alt="contact-banner" />
             </div>
             <div className="contact-container">
-                <form className="contact-form" action="/tbd" method="post">
-                <ul>
-                    <li>
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" name="user_name"/>
-                    </li>
-                    <li>
-                        <label for="mail">E-mail:</label>
-                        <input type="email" id="mail" name="user_email"/>
-                    </li>
-                    <li>
-                        <label for="msg">Message</label>
-                        <br/>
-                        <textarea id="msg" name="user_message"></textarea>
-                    </li>
-                    <li>
-                        <a class="nav-links button-links nav-button" href="/contact" >Send your message</a>
-                    </li>
-                </ul>
-                </form>    
+                <form className="contact-form">
+                    <InputHeader value="Contact Form" />
+                    <div className="input-container">
+                        <Input type="text" placeholder="Name"></Input>
+                        <Input type="email" placeholder="Email"></Input>   
+                        <Input type="textarea" placeholder="Message"></Input>
+                    </div>
+                    <a class="nav-links button-links nav-button submit-button" href="/contact" >Send your message</a>      
+                </form>
             </div>
         </Page>
     )
