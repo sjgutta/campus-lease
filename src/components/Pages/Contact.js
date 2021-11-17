68cda1c554f645c84de2951d4a6ddd42d09e87f6
Max Friedman <max.friedman0@gmail.com>
2021-11-16 18:09:41 -0800

    bad version

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -11,6 +11,28 @@ const Contact = () => {
             <div className="banner-container">
                 <img src={ContactBanner} alt="contact-banner" />
             </div>
+            <div className="contact-container">
+                <form className="contact-form" action="/tbd" method="post">
+                <ul>
+                    <li>
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="user_name"/>
+                    </li>
+                    <li>
+                        <label for="mail">E-mail:</label>
+                        <input type="email" id="mail" name="user_email"/>
+                    </li>
+                    <li>
+                        <label for="msg">Message</label>
+                        <br/>
+                        <textarea id="msg" name="user_message"></textarea>
+                    </li>
+                    <li>
+                        <a class="nav-links button-links nav-button" href="/contact" >Send your message</a>
+                    </li>
+                </ul>
+                </form>    
+            </div>
         </Page>
     )
 }
